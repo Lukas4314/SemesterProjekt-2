f033e3c8aef702e8ea822a109491fa1108cde487
Alexander <alex347a@gmail.com>
2025-02-19 13:06:04 +0100

    cam 1 til 0

--- SemesterProjekt 2/ImageFinder.cpp
+++ SemesterProjekt 2/ImageFinder.cpp
@@ -7,7 +7,7 @@ ImageFinder::ImageFinder() {}
 
 void ImageFinder::testStart() {
     // Open webcam once
-    cv::VideoCapture cap(1);
+    cv::VideoCapture cap(0);
     if (!cap.isOpened()) {
         throw std::runtime_error("Error: Could not open the webcam.");
     }
