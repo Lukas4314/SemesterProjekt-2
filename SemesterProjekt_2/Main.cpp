40bcef9519bb94d09b21b5b247a2aaabb828ca58
Karl Soneff <Karl@soneff.com>
2025-03-19 10:56:39 +0100

    Trying branch StockfishFEN

--- SemesterProjekt_2/Main.cpp
+++ SemesterProjekt_2/Main.cpp
@@ -2,7 +2,7 @@
 #include "HueDifferenceProcessor.h"
 #include "ImageFinder.h"
 #include <iostream>
-#include <windows.h>
+// #include <windows.h>
 #include <opencv2/opencv.hpp>
 #include <algorithm>
 #include "boardCutter.h"
