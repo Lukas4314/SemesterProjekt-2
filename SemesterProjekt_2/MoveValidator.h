6c3b5dcb9665f2847af32f8e5ae6deac8286b1e1
lukas4314 <lukashjeronimus@icloud.com>
2025-04-09 15:15:22 +0200

    chanhed a pointer

--- SemesterProjekt_2/MoveValidator.h
+++ SemesterProjekt_2/MoveValidator.h
@@ -24,7 +24,7 @@ private:
     static bool isValidBishop(char piece, int fromRow, int fromCol, int toRow, int toCol, const vector<vector<char>> &board);
     static bool isValidQueen(char piece, int fromRow, int fromCol, int toRow, int toCol, const vector<vector<char>> &board);
     static bool isValidKing(char piece, int fromRow, int fromCol, int toRow, int toCol, const vector<vector<char>> &board, const string &activeColor);
-    static bool underAttack(int row, int col, const string &activeColor, const vector<vector<char>> &board);
+    static bool underAttack(int row, int col, const string activeColor, const vector<vector<char>> &board);
 };
 
 #endif
