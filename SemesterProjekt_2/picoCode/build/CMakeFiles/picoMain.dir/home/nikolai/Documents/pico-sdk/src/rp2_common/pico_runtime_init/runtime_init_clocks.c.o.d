38209f2610c15b14a0506bfb24249e05c25b25f9
Nikolai <nikolaidahljensen@gmail.com>
2025-05-02 09:02:28 +0200

    pico kode motor

---
+++ SemesterProjekt_2/picoCode/build/CMakeFiles/picoMain.dir/home/nikolai/Documents/pico-sdk/src/rp2_common/pico_runtime_init/runtime_init_clocks.c.o.d
@@ -0,0 +1,52 @@
+CMakeFiles/picoMain.dir/home/nikolai/Documents/pico-sdk/src/rp2_common/pico_runtime_init/runtime_init_clocks.c.o: \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/pico_runtime_init/runtime_init_clocks.c \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/pico_runtime_init/include/pico/runtime_init.h \
+ /home/nikolai/Documents/pico-sdk/src/common/pico_base_headers/include/pico.h \
+ /home/nikolai/Documents/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /home/nikolai/Documents/pico-sdk/src/common/pico_base_headers/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/nikolai/Documents/SemesterProjekt-2/SemesterProjekt_2/picoCode/build/generated/pico_base/pico/version.h \
+ /home/nikolai/Documents/pico-sdk/src/common/pico_base_headers/include/pico/config.h \
+ /home/nikolai/Documents/SemesterProjekt-2/SemesterProjekt_2/picoCode/build/generated/pico_base/pico/config_autogen.h \
+ /home/nikolai/Documents/pico-sdk/src/boards/include/boards/pico.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/pico_platform/include/pico/platform.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/pico_platform_compiler/include/pico/platform/compiler.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/nikolai/Documents/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/pico_platform_sections/include/pico/platform/sections.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/pico_platform_panic/include/pico/platform/panic.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/nikolai/Documents/pico-sdk/src/common/pico_base_headers/include/pico/error.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/pico_runtime/include/pico/runtime.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_pll/include/hardware/pll.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/pll.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pll.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_ticks/include/hardware/ticks.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_watchdog/include/hardware/watchdog.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/watchdog.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/watchdog.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_vreg/include/hardware/vreg.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/vreg_and_chip_reset.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/vreg_and_chip_reset.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2_common/hardware_xosc/include/hardware/xosc.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/xosc.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/xosc.h \
+ /home/nikolai/Documents/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/rtc.h
--- SemesterProjekt_2/picoCode/picoMain.cpp
+++ SemesterProjekt_2/picoCode/picoMain.cpp
@@ -1,23 +1,46 @@
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/adc.h"
 
-int main() {
-    stdio_usb_init();
-    while (!stdio_usb_connected()) {
+#define PWM_PIN0 0 // Channel A
+
+int main()
+{
+    stdio_init_all();
+
+    // Initialiser ADC
+    adc_init();
+    adc_gpio_init(26); // GPIO 26 = ADC0
+    adc_select_input(0);
+
+    // Set both pins to PWM function
+    gpio_set_function(PWM_PIN0, GPIO_FUNC_PWM);
+
+    // Get slice number (same for both since pin 0 and 1 are on the same slice)
+    uint slice_num = pwm_gpio_to_slice_num(PWM_PIN0);
+
+    // Set PWM frequency to 50Hz (20ms period)
+    pwm_set_wrap(slice_num, 10);       // 1 MHz counter, so 20 ms period = 20000 counts
+    pwm_set_clkdiv(slice_num, 125.0f); // 125MHz / 125 = 1MHz
+
+    pwm_set_enabled(slice_num, true);
+
+    // Control both channels (e.g., two servos or two motor directions)
+
+    // Example: Pin 0 - open gripper, Pin 1 - close gripper
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 5); // Pin 0 - 1ms pulse (open)
+    sleep_ms(1000);
+
+    
+    while (true)
+    {
+        uint16_t result = adc_read(); // Læs ADC (0-4095)
+        printf("ADC Value: %d\n", result);
+
         sleep_ms(10);
     }
+        
 
-
-    while (true) {
-        int ch = getchar_timeout_us(0);
-        if (ch != PICO_ERROR_TIMEOUT) {
-            uint8_t received = (uint8_t)ch;
-
-
-            uint8_t response = received + 1;  // do something simple
-            putchar(response);               // send 1 byte back
-        }
-
-        sleep_ms(10); // optional: avoid busy loop
-    }
+    return 0;
 }
