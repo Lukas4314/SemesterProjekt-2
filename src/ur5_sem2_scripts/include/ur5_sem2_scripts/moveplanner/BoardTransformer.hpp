360ede5d02c9390f13f9ac13533629bc4c7ba74d
BigMagnus6Tips <magne.jacobsen04@gmail.com>
2025-05-02 15:42:02 +0200

    minor changes

--- src/ur5_sem2_scripts/include/ur5_sem2_scripts/moveplanner/BoardTransformer.hpp
+++ src/ur5_sem2_scripts/include/ur5_sem2_scripts/moveplanner/BoardTransformer.hpp
@@ -7,11 +7,23 @@
 class BoardTransformer
 {
 private:
+    const std::array<std::array<double, 4>, 4> TFRed = 
+        {{{1.0, 0.0, 0.0, 90.0},
+        {0.0, 1.0, 0.0, -30.0},
+        {0.0, 0.0, 1.0, 0.0},
+        {0.0, 0.0, 0.0, 1.0}}};
+    const std::array<std::array<double, 4>, 4> TFGreen = 
+        {{{1.0, 0.0, 0.0, 35.0},
+        {0.0, 1.0, 0.0, 25.0},
+        {0.0, 0.0, 1.0, 0.0},
+        {0.0, 0.0, 0.0, 1.0}}};
+
 public:
     BoardTransformer();
     ~BoardTransformer();
     static std::array<std::array<double, 4>, 4> getInvTFchess(double TFchess[4][4]);
     static std::array<std::array<double, 4>, 4> multiplyMatrices(double A[4][4], double B[4][4]);
+    
 };
 
 BoardTransformer::BoardTransformer()
--- src/ur5_sem2_scripts/src/robot_main.cpp
+++ src/ur5_sem2_scripts/src/robot_main.cpp
@@ -32,8 +32,8 @@ int main(int argc, char *argv[])
   
   AllInOneMain allInOneMain = AllInOneMain(camera_index);
   allInOneMain.getPieceMovedString(0);
-  std::array<std::array<double, 4>, 4> TF = allInOneMain.getBoardCutter().getTFchess();
-
+  std::array<std::array<double, 4>, 4> TFcamchess = allInOneMain.getBoardCutter().getTFchess();
+  std::array<std::array<double, 4>, 4> TFcamPlokker = allInOneMain.getBoardCutter().getTFchess();
 
   
   moveStruct move;
@@ -46,6 +46,8 @@ int main(int argc, char *argv[])
   move.captured = '-';
   move.color = 'w';
 
+
+
   double raw_TF[4][4];
   for (size_t i = 0; i < 4; ++i) {
     for (size_t j = 0; j < 4; ++j) {
@@ -54,6 +56,8 @@ int main(int argc, char *argv[])
   }
 
   // output the transformation matrix
+
+
   RCLCPP_INFO(logger, "Transformation Matrix:");
   for (size_t i = 0; i < 4; ++i) {
     std::string row_str;
@@ -63,7 +67,7 @@ int main(int argc, char *argv[])
     RCLCPP_INFO(logger, "%s", row_str.c_str());
   }
 
-  chessMoves.move(move, raw_TF);
+  // chessMoves.move(move, raw_TF);
 
   // Shutdown ROS
   rclcpp::shutdown();
