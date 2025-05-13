9c8f780c6aec931cab14fe8681a50b0ad5157f48
lukas4314 <lukashjeronimus@icloud.com>
2025-05-13 09:14:37 +0200

    defined pico pins

--- src/pico_sem2_scripts/src/Gripper.cpp
+++ src/pico_sem2_scripts/src/Gripper.cpp
@@ -4,6 +4,9 @@
 #include "hardware/pwm.h"
 #include "hardware/adc.h"
 
+#define PIN0 0
+#define PIN1 1
+
 Gripper::Gripper()
 {
     // Initialiser ADC
@@ -11,14 +14,14 @@ Gripper::Gripper()
     adc_gpio_init(26); // GPIO 26 = ADC0
     adc_select_input(0);
 
-    gpio_init(0);
-    gpio_init(1);
+    gpio_init(PIN0);
+    gpio_init(PIN1);
 
-    gpio_set_dir(0, GPIO_OUT);
-    gpio_set_dir(1, GPIO_OUT);
+    gpio_set_dir(PIN0, GPIO_OUT);
+    gpio_set_dir(PIN1, GPIO_OUT);
 
-    gpio_put(0, 0);
-    gpio_put(1, 0);
+    gpio_put(PIN0, 0);
+    gpio_put(PIN1, 0);
 }
 Gripper::~Gripper()
 {
@@ -27,14 +30,14 @@ Gripper::~Gripper()
 
 void Gripper::open()
 {
-    gpio_put(0, 1);
+    gpio_put(PIN0, 1);
     sleep_ms(1000);
-    gpio_put(0, 0);
+    gpio_put(PIN0, 0);
     putchar(1);
 }
 void Gripper::close()
 {
-    gpio_put(1, 1);
+    gpio_put(PIN1, 1);
 
     while (adc.read() < 100)
     {
@@ -42,6 +45,6 @@ void Gripper::close()
         sleep_ms(10);
     }
 
-    gpio_put(1, 0);
+    gpio_put(PIN1, 0);
     putchar(1);
 }
