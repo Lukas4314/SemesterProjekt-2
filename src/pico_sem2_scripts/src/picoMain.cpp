1d55df6e6b0fc660697982762f6007bb655b20c7
lukas4314 <lukashjeronimus@icloud.com>
2025-05-13 08:29:39 +0200

    changed to the correct picoMain.cpp

--- src/pico_sem2_scripts/src/picoMain.cpp
+++ src/pico_sem2_scripts/src/picoMain.cpp
@@ -1,61 +1,33 @@
-#include "Gripper.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/adc.h"
+#include "Gripper.h"
 
-
-Gripper::Gripper()
+int main()
 {
-   // Initialiser ADC
-   adc_init();
-   adc_gpio_init(26); // GPIO 26 = ADC0
-   adc_select_input(0);
-
-   // Set both pins to PWM function
-   gpio_set_function(PWM_PIN0, GPIO_FUNC_PWM);
-   gpio_set_function(PWM_PIN1, GPIO_FUNC_PWM);
+    stdio_init_all();
 
 
-
-
-   // Get slice number (same for both since pin 0 and 1 are on the same slice)
-   uint slice_numPin0 = pwm_gpio_to_slice_num(PWM_PIN0);
-   uint slice_numPin1 = pwm_gpio_to_slice_num(PWM_PIN1);
-
-   // Set PWM frequency to 50Hz (20ms period)
-   pwm_set_wrap(slice_numPin0, 10);       // 1 MHz counter, so 20 ms period = 20000 counts
-   pwm_set_wrap(slice_numPin1, 10);       // 1 MHz counter, so 20 ms period = 20000 counts
-
-   pwm_set_clkdiv(slice_numPin0, 125.0f); // 125MHz / 125 = 1MHz
-   pwm_set_clkdiv(slice_numPin1, 125.0f); // 125MHz / 125 = 1MHz
-
-
-   pwm_set_enabled(slice_numPin0, true);
-   pwm_set_enabled(slice_numPin1, true);
-
-}
-Gripper::~Gripper()
-{
-    // Destructor implementation
-}
-
-
-void Gripper::open(){
-    pwm_set_chan_level(slice_numPin0, PWM_CHAN_A, 5); // Pin 0 - 1ms pulse (open)
-    sleep_ms(1000);
-    pwm_set_chan_level(slice_numPin0, PWM_CHAN_A, 0); // Pin 0 - 1ms pulse (open)
-    putchar(1);
-
-}
-void Gripper:close(){
-    pwm_set_chan_level(slice_numPin1, PWM_CHAN_A, 5); // Pin 0 - 2ms pulse (close)
-
-    while (adc.read() < 100) {
-        
+    stdio_usb_init();
+    while (!stdio_usb_connected()) {
         sleep_ms(10);
     }
 
-    pwm_set_chan_level(slice_numPin1, PWM_CHAN_A, 0); // Pin 0 - 2ms pulse (close)
-    putchar(1);
+    Gripper gripper;
+
+
+    while (true) {
+        int ch = getchar_timeout_us(0);
+        if (ch != PICO_ERROR_TIMEOUT) {
+            if (ch == 1){
+                gripper.open();
+            }
+            else if (ch == 2){
+                gripper.close();
+            }
+        }
+        sleep_ms(10); // optional: avoid busy loop
+    }
+    return 0;
 }
