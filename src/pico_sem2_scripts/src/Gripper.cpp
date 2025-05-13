509ceccceb924fa7bd40dc29938a26142d562b7e
lukas4314 <lukashjeronimus@icloud.com>
2025-05-13 08:56:27 +0200

    made the gripper work with digital instead of PWM

--- src/pico_sem2_scripts/src/Gripper.cpp
+++ src/pico_sem2_scripts/src/Gripper.cpp
@@ -4,58 +4,44 @@
 #include "hardware/pwm.h"
 #include "hardware/adc.h"
 
-
 Gripper::Gripper()
 {
-   // Initialiser ADC
-   adc_init();
-   adc_gpio_init(26); // GPIO 26 = ADC0
-   adc_select_input(0);
+    // Initialiser ADC
+    adc_init();
+    adc_gpio_init(26); // GPIO 26 = ADC0
+    adc_select_input(0);
 
-   // Set both pins to PWM function
-   gpio_set_function(PWM_PIN0, GPIO_FUNC_PWM);
-   gpio_set_function(PWM_PIN1, GPIO_FUNC_PWM);
+    gpio_init(0);
+    gpio_init(1);
 
+    gpio_set_dir(0, GPIO_OUT);
+    gpio_set_dir(1, GPIO_OUT);
 
-
-
-   // Get slice number (same for both since pin 0 and 1 are on the same slice)
-   uint slice_numPin0 = pwm_gpio_to_slice_num(PWM_PIN0);
-   uint slice_numPin1 = pwm_gpio_to_slice_num(PWM_PIN1);
-
-   // Set PWM frequency to 50Hz (20ms period)
-   pwm_set_wrap(slice_numPin0, 10);       // 1 MHz counter, so 20 ms period = 20000 counts
-   pwm_set_wrap(slice_numPin1, 10);       // 1 MHz counter, so 20 ms period = 20000 counts
-
-   pwm_set_clkdiv(slice_numPin0, 125.0f); // 125MHz / 125 = 1MHz
-   pwm_set_clkdiv(slice_numPin1, 125.0f); // 125MHz / 125 = 1MHz
-
-
-   pwm_set_enabled(slice_numPin0, true);
-   pwm_set_enabled(slice_numPin1, true);
-
+    gpio_put(0, 0);
+    gpio_put(1, 0);
 }
 Gripper::~Gripper()
 {
     // Destructor implementation
 }
 
-
-void Gripper::open(){
-    pwm_set_chan_level(slice_numPin0, PWM_CHAN_A, 5); // Pin 0 - 1ms pulse (open)
+void Gripper::open()
+{
+    gpio_put(0, 1);
     sleep_ms(1000);
-    pwm_set_chan_level(slice_numPin0, PWM_CHAN_A, 0); // Pin 0 - 1ms pulse (open)
+    gpio_put(0, 0);
     putchar(1);
-
 }
-void Gripper::close(){
-    pwm_set_chan_level(slice_numPin1, PWM_CHAN_A, 5); // Pin 0 - 2ms pulse (close)
+void Gripper::close()
+{
+    gpio_put(1, 1);
 
-    while (adc.read() < 100) {
-        
+    while (adc.read() < 100)
+    {
+
         sleep_ms(10);
     }
 
-    pwm_set_chan_level(slice_numPin1, PWM_CHAN_A, 0); // Pin 0 - 2ms pulse (close)
+    gpio_put(1, 0);
     putchar(1);
 }
