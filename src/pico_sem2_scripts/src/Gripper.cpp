aaf1a6e625739dc90ed61a73f908aa5ffdeede96
lukas4314 <lukashjeronimus@icloud.com>
2025-05-14 09:34:44 +0200

    made it work with the merged pico-ros

--- src/pico_sem2_scripts/src/Gripper.cpp
+++ src/pico_sem2_scripts/src/Gripper.cpp
@@ -1,50 +1,42 @@
 #include "Gripper.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
 #include "hardware/adc.h"
 
-#define PIN0 0
-#define PIN1 1
 
 Gripper::Gripper()
 {
-    // Initialiser ADC
-    adc_init();
-    adc_gpio_init(26); // GPIO 26 = ADC0
-    adc_select_input(0);
+   // Initialiser ADC
+   adc_init();
+   adc_gpio_init(26); // GPIO 26 = ADC0
+   adc_select_input(0);
 
-    gpio_init(PIN0);
-    gpio_init(PIN1);
+   // Pin configuration
+    gpio_init(PIN14);
+    gpio_set_dir(PIN14, GPIO_OUT);
 
-    gpio_set_dir(PIN0, GPIO_OUT);
-    gpio_set_dir(PIN1, GPIO_OUT);
+    gpio_init(PIN15);
+    gpio_set_dir(PIN15, GPIO_OUT);
 
-    gpio_put(PIN0, 0);
-    gpio_put(PIN1, 0);
 }
 Gripper::~Gripper()
 {
     // Destructor implementation
 }
 
-void Gripper::open()
-{
-    gpio_put(PIN0, 1);
-    sleep_ms(1000);
-    gpio_put(PIN0, 0);
-    putchar(1);
+
+void Gripper::open(){
+    gpio_put(PIN14, 1);
+//   sleep_ms(2000);
+//    gpio_put(PIN0, 0);
 }
-void Gripper::close()
-{
-    gpio_put(PIN1, 1);
 
-    while (adc.read() < 100)
-    {
+void Gripper::close(){
+    gpio_put(PIN14, 1);
 
+    while (adc_read() < 100) {
+        
         sleep_ms(10);
     }
-
-    gpio_put(PIN1, 0);
-    putchar(1);
+    gpio_put(PIN15, 0);
 }
